90ba2abe7af26763c30fe0ed2393be3e0760a3e8
Ankith <105031597+Ankith008@users.noreply.github.com>
2024-05-02 23:33:19 +0530

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connecttodb = require("./db");
 const cookieParser = require("cookie-parser");
 connecttodb();
 app.use(express.json());
-app.use(cors({ origin: "https://brain-assignment.vercel.app/" }));
+
 app.use(cookieParser());
 app.options("*",cors())
 app.use("/auth", require("./routes/auth"));
