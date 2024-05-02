64b8c8a8f7479fccff781aa01471f3cfc106c03d
Ankith <105031597+Ankith008@users.noreply.github.com>
2024-05-02 23:31:43 +0530

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ connecttodb();
 app.use(express.json());
 app.use(cors({ origin: "https://brain-assignment.vercel.app/" }));
 app.use(cookieParser());
-
+app.options("*",cors())
 app.use("/auth", require("./routes/auth"));
 app.use("/post", require("./routes/post"));
 app.listen(5000);
